6ec83f0ed80750c3b72ef957586393167296ef9f
Nikita-Shirbhate <140882209+Nikita-Shirbhate@users.noreply.github.com>
2025-04-19 00:56:33 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,2 +1,13 @@
-subnet_ids = aws_subnet.public[*].id
+resource "aws_lb" "simpletime_alb" {
+  name               = "simpletime-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public[*].id   # <- this is correct inside this block
+  security_groups    = [aws_security_group.lb_sg.id]
+
+  tags = {
+    Name = "simpletime-alb"
+  }
+}
+
 
